5d26b026a4a18173642a0f9873671ef483939d3f
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-12 22:53:11 -0500

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
+import Carousel from './components/Carousel';
 import React from 'react';
 import './App.scss';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <NavigationBar />
       <Header />
+      <Carousel />
     </div>
   );
 }
---
+++ src/components/Carousel.js
@@ -0,0 +1,57 @@
+import { Carousel } from 'reactstrap';
+import { useState } from 'react';
+import slideImageOne from '../app/assets/img/purplebg.jpg';
+import slideImageTwo from '../app/assets/img/mauvebg.jpg';
+import slideImageThree from '../app/assets/img/greenbg.jpg';
+
+ Carousel = () => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slideImageOne}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slideImageTwo}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slideImageThree}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+
+};
+
+export default Carousel; 
